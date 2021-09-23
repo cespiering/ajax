99f0741ea2ee10ec322834de4f3d7ce61126f7ab
cespiering <59517544+cespiering@users.noreply.github.com>
2021-09-22 17:03:30 -0700

    working ajax

--- server.py
+++ server.py
@@ -39,7 +39,9 @@ def index():
 def fortune():
     """Return a single fortune as a text string (*not* the whole HTML page!)"""
 
-    return random.choice(FORTUNES)
+    fortune = random.choice(FORTUNES)
+
+    return jsonify(fortune)
 
 
 @app.route('/weather.json')
--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -4,6 +4,9 @@
 
 function showFortune(evt) {
   // TODO: get the fortune and show it in the #fortune-text div
+  $.get('/fortune', (res) => {
+    $('#fortune-text').html(res);
+  });
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -15,9 +18,12 @@ function showWeather(evt) {
 
   const url = '/weather.json';
   const formData = {zipcode: $('#zipcode-field').val()};
-
+  $.get(url, formData, (response) => {
+    $('#weather-form').html(response["forecast"])
+  });
+}    
   // TODO: request weather with that URL and show the forecast in #weather-info
-}
+
 
 $('#weather-form').on('submit', showWeather);
 
@@ -25,9 +31,19 @@ $('#weather-form').on('submit', showWeather);
 
 function orderMelons(evt) {
   evt.preventDefault();
+  const url = '/order-melons.json';
+  const formData = {qty: $('#qty-field').val(), melon_type: $('#melon-type-field').val()};
+
+  $.post(url, formData, (response) => {
+    if (response['code'] === "ERROR") {
+      $('#order-status').addClass('order-error');
+    }
+    $('#order-status').text(response['code'], response['msg']);
+  });
+}
 
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+
 
 $('#order-form').on('submit', orderMelons);
